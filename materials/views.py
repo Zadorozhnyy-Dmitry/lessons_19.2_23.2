923c444858c221f888005d3fe8f517e27e0ad42d
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-09-14 00:33:01 +0300

    21.1 (5) Создаем новую модель

---
+++ materials/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+
+from materials.models import Material
+from django.urls import reverse_lazy
+
+
+class MaterialCreateView(CreateView):
+    model = Material
+    fields = ('title', 'body',)
+    success_url = reverse_lazy('materials:list')
+
+
+class MaterialUpdateView(UpdateView):
+    model = Material
+    fields = ('title', 'body',)
+    success_url = reverse_lazy('materials:list')
+
+
+class MaterialListView(ListView):
+    model = Material
+
+
+class MaterialDetailView(DetailView):
+    model = Material
+
+
+class MaterialDeleteVIew(DeleteView):
+    model = Material
+    success_url = reverse_lazy('materials:list')
