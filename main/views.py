1cdb4cd8dd570f239efead0cc452d4f6250335d1
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-09-13 22:27:44 +0300

    20.2 (5) Подшаблоны и базовые шаблоны

--- main/views.py
+++ main/views.py
@@ -7,16 +7,19 @@ def index(request):
     students_list = Student.objects.all()
     context = {
         'object_list': students_list,
-
+        'title': 'Главная'
     }
     return render(request, 'main/index.html', context)
 
 
-# def index(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         email = request.POST.get('email')
-#         message = request.POST.get('message')
-#         print(f'{name} ({email}): {message}')
-#         return render(request, 'main/index.html')
-#
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} ({email}): {message}')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'main/contact.html', context)
+
