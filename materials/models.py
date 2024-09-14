5b4600503051551e7cf6d3ae30e3c68be7c2efbe
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-09-14 08:46:51 +0300

    21.1 (7) Реализуем счетчик просмотров

--- materials/models.py
+++ materials/models.py
@@ -5,6 +5,10 @@ class Material(models.Model):
     title = models.CharField(max_length=150, verbose_name='название')
     body = models.TextField(verbose_name='содержимое')
 
+    views_count = models.IntegerField(default=0, verbose_name='просмотры')
+    is_published = models.BooleanField(default=True, verbose_name='опубликовано')
+    slug = models.CharField(max_length=150, verbose_name='slug', blank=True, null=True)
+
     def __str__(self):
         return f'{self.title}'
 
